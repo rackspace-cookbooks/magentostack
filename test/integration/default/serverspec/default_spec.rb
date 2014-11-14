2ba7078dab262835fdc75b5ee2705ff1cdc59d26
Julien Berard <jujugrrr@gmail.com>
2014-11-14 17:12:57 +0000

    Added integration tests for apache-fpm

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -2,15 +2,17 @@
 
 require_relative 'spec_helper'
 
-# apache
+# Apache-fpm
 if os[:family] == 'redhat'
   describe service('httpd') do
     it { should be_enabled }
+    it { should be_running }
   end
   apache2ctl = '/usr/sbin/apachectl'
 else
   describe service('apache2') do
     it { should be_enabled }
+    it { should be_running }
   end
   apache2ctl = '/usr/sbin/apache2ctl'
 end
@@ -20,8 +22,46 @@ end
 describe port(443) do
   it { should be_listening }
 end
+
+describe service('php-fpm') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+%w(
+  status actions alias auth_basic
+  authn_file authz_default
+  authz_groupfile authz_host
+  authz_user autoindex dir env mime
+  negotiation setenvif ssl headers
+  expires log_config logio fastcgi
+).each do |mod|
+  describe command("#{apache2ctl} -M") do
+    its(:stdout) { should match(/^ #{mod}_module/) }
+  end
+end
+
 describe command("#{apache2ctl} -M") do
   its(:stdout) { should match(/^ ssl_module/) }
+end
+
+describe command("#{apache2ctl} -t") do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("#{apache2ctl} -S") do
+  its(:stdout) { should match(/port 443 namevhost mymagento.com/) }
+  its(:stdout) { should match(/port 80 namevhost mymagento.com/) }
+end
+
+if os[:family] == 'redhat'
+  describe file('/var/www/html/magento') do
+    it { should be_directory }
+  end
+else
+  describe file('/var/www/magento') do
+    it { should be_directory }
+  end
 end
 
 # redis
