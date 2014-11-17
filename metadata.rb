a9fa66252fe6c00b808660c5a01bfdaf0f79e010
Julien Berard <jujugrrr@gmail.com>
2014-11-17 10:24:34 +0000

    Updated Readme

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer 'Rackspace'
 maintainer_email 'rackspace-cookbooks@rackspace.com'
 license 'Apache 2.0'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-description 'Provides a full php stack'
+description 'Provides a full Magento stack'
 version '0.0.1'
 
 depends 'apache2'
