97a14a623bb5031e57698a4d527f1d0843077911
seeafish <myself@seeafish.me>
2014-11-18 16:58:15 +0000

    Added mysql server spec tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -37,3 +37,19 @@ end
 describe port(6379) do
   it { should be_listening }
 end
+
+# mysql base
+if os[:family] == 'redhat'
+  describe service('mysqld') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+else
+  describe service('mysql') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
+describe port(3306) do
+  it { should be_listening }
+end
