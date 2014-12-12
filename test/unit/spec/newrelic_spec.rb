ece0ea1a8941412bfe9e71ec30d7088c2e5ab5ab
Sia Gholami <myself@seeafish.me>
2014-12-12 18:32:22 +0000

    Added unit test

---
+++ test/unit/spec/newrelic_spec.rb
@@ -0,0 +1,24 @@
+# Encoding: utf-8
+
+require_relative 'spec_helper'
+
+describe 'magentostack::newrelic' do
+  before { stub_resources }
+  supported_platforms.each do |platform, versions|
+    versions.each do |version|
+      context "on #{platform.capitalize} #{version}" do
+
+        let(:chef_run) do
+          ChefSpec::ServerRunner.new(platform: platform, version: version, log_level: ::LOG_LEVEL) do |node, server|
+            node_resources(node)
+          end.converge(described_recipe)
+        end
+
+        it 'includes recipes' do
+          expect(chef_run).to include_recipe('stack_commons::newrelic')
+        end
+
+      end
+    end
+  end
+end
