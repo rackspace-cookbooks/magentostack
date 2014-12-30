e898ad1b2536fb907fc19a21639acfff7ac5f6dc
Martin Smith <martin@mbs3.org>
2014-12-30 14:39:10 -0600

    Revert "Can't check for 10.0.0.0/8 IPs"
    This reverts commit 420803ec1fdf8d119726c5baa7f841ab090e2eb1. This is a unit test so checking for iptables rules should be fine.

--- test/unit/spec/redis_spec.rb
+++ test/unit/spec/redis_spec.rb
@@ -41,6 +41,13 @@ describe 'magentostack all in one demo' do
             expect(chef_run).to include_recipe(recipe)
           end
         end
+
+        it 'should generate appropriate rackspace_iptables calls' do
+          node = chef_run.node
+          ipt = node['rackspace_iptables']['config']['chains']['INPUT']
+
+          expect(ipt['-m tcp -p tcp -s 10.0.0.2 --dport 6379 -j ACCEPT']).to be_truthy
+        end
       end
     end
   end
@@ -88,6 +95,15 @@ describe 'magentostack expanded all-in-one' do
           ).each do |recipe|
             expect(chef_run).to include_recipe(recipe)
           end
+        end
+
+        it 'should generate appropriate rackspace_iptables calls' do
+          node = chef_run.node
+          ipt = node['rackspace_iptables']['config']['chains']['INPUT']
+
+          expect(ipt['-m tcp -p tcp -s 10.0.0.2 --dport 6381 -j ACCEPT']).to be_truthy
+          expect(ipt['-m tcp -p tcp -s 10.0.0.2 --dport 6383 -j ACCEPT']).to be_truthy
+          expect(ipt['-m tcp -p tcp -s 10.0.0.2 --dport 6385 -j ACCEPT']).to be_truthy
         end
       end
     end
