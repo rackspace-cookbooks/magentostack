d9114816c914f7fac05c1b5a4dec390d3d1bdc7b
Martin Smith <martin@mbs3.org>
2014-12-31 15:01:02 -0600

    Fix more tests with s/80/8080/g and s/443/8443/g

--- test/integration/default-expanded/serverspec/default_spec.rb
+++ test/integration/default-expanded/serverspec/default_spec.rb
@@ -24,10 +24,10 @@ describe service(fpm_service_name) do
   it { should be_enabled }
   it { should be_running }
 end
-describe port(80) do
+describe port(8080) do
   it { should be_listening }
 end
-describe port(443) do
+describe port(8443) do
   it { should be_listening }
 end
 
@@ -54,13 +54,13 @@ end
 ## apachectl -S on Apache 2.4(default on Ubuntu 14) has a different output
 if os[:release] == '14.04'
   describe command("#{apache2ctl} -S") do
-    its(:stdout) { should match(/\*:443                  localhost/) }
-    its(:stdout) { should match(/\*:80                   localhost/) }
+    its(:stdout) { should match(/\*:8443                  localhost/) }
+    its(:stdout) { should match(/\*:8080                   localhost/) }
   end
 else
   describe command("#{apache2ctl} -S") do
-    its(:stdout) { should match(/port 443 namevhost localhost/) }
-    its(:stdout) { should match(/port 80 namevhost localhost/) }
+    its(:stdout) { should match(/port 8443 namevhost localhost/) }
+    its(:stdout) { should match(/port 8080 namevhost localhost/) }
   end
 end
 
