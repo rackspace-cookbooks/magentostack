56c70ee9e62df2dbd7665dc1709f59f874134ce2
Martin Smith <martin@mbs3.org>
2015-01-12 15:24:15 -0600

    Drastically simplify redis cache testing
    - Added shared_examples for redis caches, so that the tests themselves are dramatically shorter
    - Ran into a problem where tests running concurrently were breaking each other if they cleared out redis, so I moved the clearing out all together in a before-do/end block.
    - Rubocop cleanup

--- test/integration/default-expanded/serverspec/caches_spec.rb
+++ test/integration/default-expanded/serverspec/caches_spec.rb
@@ -1,49 +1,13 @@
 require_relative 'spec_helper'
 
-redis_path = '/usr/local/bin'
-
 # object cache (magento calls this the 'cache', in redis db 0)
-describe 'redis instance for object cache' do
-  # empty database 0
-  describe command("#{redis_path}/redis-cli -a runstatepasswordobject -p 6383 --no-raw -n 0 flushdb") do
-    its(:exit_status) { should eq(0) }
-  end
-
-  # ensure there are no keys
-  describe command("#{redis_path}/redis-cli -a runstatepasswordobject -p 6383 --no-raw -n 0 keys '*'") do
-    its(:stdout) { should match(/empty list or set/) }
-  end
-
-  # page hit to populate caches
-  describe command('wget -qO- localhost:8080') do
-    its(:stdout) { should match(/Magento Demo Store/) }
-  end
-
-  # ensure the cache is non-empty now
-  describe command("#{redis_path}/redis-cli -a runstatepasswordobject -p 6383 --no-raw -n 0 keys '*'") do
-    its(:stdout) { should_not match(/empty list or set/) }
-  end
-end
-
 # session cache (magento calls this the 'cache', in redis db 2)
-describe 'redis instance for session cache' do
-  # empty database 0
-  describe command("#{redis_path}/redis-cli -a runstatepasswordsession -p 6381 --no-raw -n 2 flushdb") do
-    its(:exit_status) { should eq(0) }
+describe 'redis instance for object cache' do
+  before do
+    clear_out_redis '-a runstatepasswordobject -n 0'
+    clear_out_redis '-a runstatepasswordsession -n 2'
+    page_returns
   end
-
-  # ensure there are no keys
-  describe command("#{redis_path}/redis-cli -a runstatepasswordsession -p 6381 --no-raw -n 2 keys '*'") do
-    its(:stdout) { should match(/empty list or set/) }
-  end
-
-  # page hit to populate caches
-  describe command('wget -qO- localhost:8080') do
-    its(:stdout) { should match(/Magento Demo Store/) }
-  end
-
-  # ensure the cache is non-empty now
-  describe command("#{redis_path}/redis-cli -a runstatepasswordsession -p 6381 --no-raw -n 2 keys '*'") do
-    its(:stdout) { should_not match(/empty list or set/) }
-  end
+  it_behaves_like 'magento redis cache', '-p 6383 -a runstatepasswordobject -n 0'
+  it_behaves_like 'magento redis cache', '-p 6381 -a runstatepasswordsession -n 2'
 end
