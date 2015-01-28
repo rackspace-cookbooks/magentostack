21064460ff99e3fb5194747eb8d89b8e10dd13ea
Martin Smith <martin@mbs3.org>
2015-01-28 02:54:55 -0600

    Introduce NFS client and server recipes
    Use client and server recipes to symlink /mnt/magento_media to /var/www/html/magento/media.

--- test/integration/community/serverspec/magento_spec.rb
+++ test/integration/community/serverspec/magento_spec.rb
@@ -6,4 +6,8 @@ describe 'Magento web' do
   it 'can connect to magento' do
     expect(page_returns('https://localhost:8443/', 'localhost', true)).to match(/You have no items in your shopping cart./)
   end
+
+  describe 'community magento' do
+    it_behaves_like 'magento community edition'
+  end
 end
