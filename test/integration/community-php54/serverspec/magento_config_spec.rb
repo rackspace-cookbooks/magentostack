16d30c30e54f578e59dc3be8b7dd37dd0162d763
Martin Smith <martin@mbs3.org>
2015-04-17 13:06:50 -0500

    Remove additional redundant test suites for TK
    - Assume php54 and v113 can be tested together
    - Assume community-redis is a subset of enterprise-redis, only test the latter

--- test/integration/community-php54/serverspec/magento_config_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'Magentostack xml configuration' do
-  describe 'Redis password for session' do
-    describe file('/var/www/html/magento/app/etc/local.xml') do
-      its(:content) { should match %r{<password>runstatepasswordsingle</password>} }
-    end
-  end
-end
