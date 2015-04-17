f19fe9e936d84d90c79bdd86d10f6502949dc203
Martin Smith <martin@mbs3.org>
2015-04-17 08:46:15 -0500

    Fix tests for default virtualhost
    - In order to make CI pass, fix the tests to check the correct vhost files.
    - Shorten many unit tests

--- test/unit/spec/magento_admin_spec.rb
+++ test/unit/spec/magento_admin_spec.rb
@@ -8,10 +8,8 @@ describe 'magentostack::magento_admin' do
     versions.each do |version|
       context "on #{platform.capitalize} #{version}" do
         cached(:chef_run) do
-          ChefSpec::ServerRunner.new(platform: platform, version: version) do |node, server|
+          ChefSpec::SoloRunner.new(platform: platform, version: version) do |node|
             node_resources(node) # stub this node
-            stub_nodes(platform, version, server) # stub other nodes for chef-zero
-            stub_environments(server)
 
             # Stub the node and any calls to Environment.Load to return this environment
             env = Chef::Environment.new
